e99da71d45a3373e3b40dd5a40a64162a5abfa9e
LUIS FAUTH <luisdfauth@usp.br>
2024-11-14 07:01:05 -0300

    Primeiro commit

---
+++ staticpages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {}
+    return render(request, 'staticpages/index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'staticpages/about.html', context)
+
+# Create your views here.
